3837859bc3e4ff0f768d77daa153793d97edc38a
daryll <dosis@csumb.edu>
2018-06-09 11:44:27 -0700

    initial commit

---
+++ navshrink.js
@@ -0,0 +1,54 @@
+$(window).scroll(function() {
+	if($(document).scrollTop() > 50) {
+		$('nav').addClass('shrink');
+	} else {
+		$('nav').removeClass('shrink');
+	}
+});
+
+$(document).ready(function(){
+	$("#link0").click(function(){
+        $path=$("#homediv").offset().top;
+        $('html, body').animate({scrollTop:$path},1000);
+   });
+   $("#link1").click(function(){
+        $path=$("#aboutdiv").offset().top;
+        $('html, body').animate({scrollTop:$path},1000);
+   });
+   $("#link2").click(function(){
+        $path=$("#educationdiv").offset().top;
+        $('html, body').animate({scrollTop:$path},1000);
+   });
+   $("#link3").click(function(){
+        $path=$("#projectdiv").offset().top;
+        $('html, body').animate({scrollTop:$path},1000);
+   });
+   $("#link4").click(function(){
+        $path=$("#experiencediv").offset().top;
+        $('html, body').animate({scrollTop:$path},1000);
+   });
+   $("#link5").click(function(){
+        $path=$("#homediv").offset().top;
+        $('html, body').animate({scrollTop:$path},1000);
+   });
+
+});
+
+
+
+// for navigation bar when it's mobile size
+$(document).ready(function() {
+	$(".menu-icon").on("click", function() {
+		$("nav ul").toggleClass("showing");
+	});
+});
+
+// scrolling effect for nav bar
+$(window).on("scroll", function() {
+	if($(window).scrollTop() > 20) {
+		$('nav').addClass('black');
+	} else {
+		$('nav').removeClass('black');
+	}
+})
+
